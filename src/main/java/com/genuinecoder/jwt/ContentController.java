a4eb758fec06be822bb730b800f2c421506aec3b
Mayur <107352861+Mayurdpatil67@users.noreply.github.com>
2025-04-17 20:58:02 +0530

    ContentController with JWT-based authentication and role-specific routes

--- src/main/java/com/genuinecoder/jwt/ContentController.java
+++ src/main/java/com/genuinecoder/jwt/ContentController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ContentController {
+public class ContentController{
 
     @Autowired
     private AuthenticationManager authenticationManager;
