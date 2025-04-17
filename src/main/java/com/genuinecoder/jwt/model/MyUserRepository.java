7dc72bd7b404a64db25fef70f76bd2b15dc6dbbf
Mayur <107352861+Mayurdpatil67@users.noreply.github.com>
2025-04-17 20:54:14 +0530

    MyUserRepository with method to find user by username

--- src/main/java/com/genuinecoder/jwt/model/MyUserRepository.java
+++ src/main/java/com/genuinecoder/jwt/model/MyUserRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MyUserRepository extends JpaRepository<MyUser, Long> {
+public interface MyUserRepository extends JpaRepository<MyUser, Long>{
 
     Optional<MyUser> findByUsername(String username);
 }
