7adcfc3b5ed92b1fa5864bb5b4440826a37feb79
Mayur <107352861+Mayurdpatil67@users.noreply.github.com>
2025-04-17 20:52:04 +0530

    MyUser entity with basic fields

--- src/main/java/com/genuinecoder/jwt/model/MyUser.java
+++ src/main/java/com/genuinecoder/jwt/model/MyUser.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class MyUser {
+public class MyUser{
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
